34d8d8c39f1dd89d59406718dd1d49b2d5dd0fc1
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-06-13 04:17:55 +0530

    Add files via upload

---
+++ Pong/Pong Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pong Watch App
+//
+//  Created by Ganesh on 05/06/23.
+//
+
+import SwiftUI
+import SpriteKit
+
+struct ContentView: View {
+    @State private var crownPosition = 0.0
+    var body: some View {
+        GeometryReader { reader in
+            SpriteView(scene: PongScene(size: reader.size, crownPosition: $crownPosition))
+                .focusable()
+                .digitalCrownRotation($crownPosition)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pong/Pong Watch App/PongApp.swift
@@ -0,0 +1,17 @@
+//
+//  PongApp.swift
+//  Pong Watch App
+//
+//  Created by Ganesh on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Pong_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
